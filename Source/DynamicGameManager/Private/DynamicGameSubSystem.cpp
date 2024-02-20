0f5e8b6ddf1cd326ef5827e7e365f6f771fd78f5
wangpeng <wpp1983@gmail.com>
2024-02-20 16:09:39 +0800

    Init

---
+++ Source/DynamicGameManager/Private/DynamicGameSubSystem.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DynamicGameSubSystem.h"
+
+bool UDynamicGameSubSystem::LoadGame(const FString& GamePath)
+{
+	
+	IModuleManager& ModuleManager = FModuleManager::Get();
+
+	FName ModuleName = TEXT("YourModuleName"); // 模块的名字
+
+	// 检查模块是否已经加载
+	if (!ModuleManager.IsModuleLoaded(ModuleName))
+	{
+		// 动态加载模块
+		IModuleInterface* YourModule = ModuleManager.LoadModule(ModuleName);
+		if (YourModule)
+		{
+			// 模块加载成功，可以进行后续操作
+		}
+		else
+		{
+			// 模块加载失败，可以进行错误处理
+			return false;
+		}
+	}
+	return true;
+}
---
+++ Source/DynamicGameManager/Public/DynamicGameSubSystem.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "DynamicGameSubSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNAMICGAMEMANAGER_API UDynamicGameSubSystem : public UEngineSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	bool DownloadGame();
+	
+	bool LoadGame(const FString& GamePath);
+	
+};
